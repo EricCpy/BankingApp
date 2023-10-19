643a4b0e8642b0cb8aeaf5ca2ea706b3df96e432
Eric <heimleiter.dasheim@gmail.com>
2023-10-19 13:05:09 +0200

    setup backend and frontend

---
+++ bankingapp/src/test/java/de/eric/bankingapp/BankingappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eric.bankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
